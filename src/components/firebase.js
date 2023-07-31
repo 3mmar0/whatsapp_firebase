59022ec5fd81eb0c14700677f39cf74cdf00771b
3mmar0 <ammarelgndy6@gmail.com>
2023-07-31 22:19:14 +0300

    test

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,28 +1,19 @@
 import firebase from "firebase/compat/app";
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyCS8AGASrLfecMzFUA6-5jzfSytxGhkPM4",
-    authDomain: "whatsapp-cl-fb.firebaseapp.com",
-    projectId: "whatsapp-cl-fb",
-    storageBucket: "whatsapp-cl-fb.appspot.com",
-    messagingSenderId: "743231490986",
-    appId: "1:743231490986:web:be01a7a45959c21229f9d3",
-    measurementId: "G-Y8MYXRX8C2"
+  apiKey: "AIzaSyB5cTKtukAqkRmnMeXr_p5v52TbPTuYbZs",
+  authDomain: "whatsapp-cl-react.firebaseapp.com",
+  projectId: "whatsapp-cl-react",
+  storageBucket: "whatsapp-cl-react.appspot.com",
+  messagingSenderId: "202490231707",
+  appId: "1:202490231707:web:07cee4b99d82cae9663f3e",
 });
 
 const db = firebaseApp.firestore();
 const auth = firebaseApp.auth();
 const provider = new firebase.auth.GoogleAuthProvider();
 
-export {auth, provider};
+export { auth, provider };
 export default db;
-
-
-{/* <BrowserRouter>
-    <Sidebar />
-    <Routes>
-    <Route path='/rooms/:roomId' element={<Chat />} />
-    </Routes>
-</BrowserRouter> */}
